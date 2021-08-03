de324b3e4ec7e8ea26560089f73e46c141086d59
Nji-Mariette <marietteneh@gmail.com>
2021-08-03 03:55:25 -0700

    updated <exercise_40.py>

--- Mariette/exercise_40.py
+++ Mariette/exercise_40.py
@@ -1,14 +1,11 @@
 def bubblesort(arr):
-    k=len(arr)
 
-    for i in range (k-1):
-        for j in range (0,k-i-1):
+
+    for i in range (len(arr)-1):
+        for j in range (0,len(arr)-i-1):
             if arr[j]> arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
 
-arr = [76,78,34,21,22,19]
+    return arr
 
-bubblesort(arr)
-print("sorted array is")
-for i in range (len(arr)):
-    print("% d"% arr[i])
+print(bubblesort(arr = [76,78,34,21,22,19]))
