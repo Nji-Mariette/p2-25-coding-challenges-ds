441873fe37ae13aaf0bcb32ba1ff9e2c102c6287
Nji-Mariette <marietteneh@gmail.com>
2021-08-03 03:27:34 -0700

    Done with <exercise_26.py>

--- Mariette/exercise_26.py
+++ Mariette/exercise_26.py
@@ -1,13 +1,11 @@
-arr1=[1,2,3,4,5]
-arr2=[1,2,4,6,8,10]
-k=len(arr1)
-m=len(arr2)
-def findMissing(arr1,ar2,k,m):
-    for i in range (k):
-        for j in range (m):
-            if (arr1[i]==arr2[j]):
-                break
-        if (j == m-1):
-            print(arr1[i],end=" ")
-    findMissing(arr1,arr2,k,m)
+
+
+
+
+
+a1=[2,4,5,9]
+a2=[1,4,3,2]
+def elt_in_arr1(a1,a2):
+        return [x for x in a1 if x not in a2]
+print(elt_in_arr1(a1,a2))
 
